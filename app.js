4c686f207a88d72f6c33a5707a068eb33492adce
Im-Hassan-wd <alhassansahih@gmail.com>
2021-07-13 15:53:36 -0400

    last commit

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 const faqList = document.querySelector('ul');
 
 faqList.addEventListener('click', e => {
-  if(e.target.tagName === 'IMG'){
+  if(e.target.tagName === 'IMG' || e.target.tagName === 'P'){
     e.target.classList.toggle('active');
     if(e.target.classList.contains( 'active')){
       e.target
       .parentElement
       .parentElement
       .children[1]
-      .style.display = 'block';
+      .style.height = 'initial';
     }else {
       e.target
       .parentElement
       .parentElement
       .children[1]
-      .style.display = 'none';
+      .style.height = '0';
     }
   }
 })
